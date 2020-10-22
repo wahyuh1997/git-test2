f0130186ab80f3e73c0ad899deff5bb84331231f
wahyuh1997 <wahyuh1997@gmail.com>
2020-10-22 15:08:31 +0700

    Menambahkan Paragraf

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, sit velit odit, deleniti repudiandae reprehenderit veritatis magnam eveniet adipisci repellat possimus enim? Excepturi, error beatae! Minima, nemo ab? Perferendis, omnis.</p>
+</body>
+
+</html>
